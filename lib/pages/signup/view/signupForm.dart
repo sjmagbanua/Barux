93d2e0c6c79ca8c2873c99b0cfbca5796e191d2e
sjmagbanua <snickersjay10@gmail.com>
2025-07-30 18:57:27 +0800

    wrap blocprovider signupage & blocbuilder signupform

--- lib/pages/signup/view/signupForm.dart
+++ lib/pages/signup/view/signupForm.dart
@@ -1,79 +1,103 @@
+import 'package:barux/pages/signup/bloc/bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class Signupform extends StatelessWidget {
   const Signupform({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        TextFormField(
-          textAlign: TextAlign.left,
-          decoration: InputDecoration(
-            fillColor: Colors.red,
-            hintText: 'Full Name',
-            prefixIcon: Icon(
-              Icons.account_circle_outlined,
-            ),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(
-            vertical: 16,
-          ),
-          child: TextFormField(
-            textAlign: TextAlign.left,
-            decoration: InputDecoration(
-              fillColor: Colors.red,
-              hintText: 'Phone or Email',
-              prefixIcon: Icon(
-                Icons.email_outlined,
-              ),
-            ),
-          ),
-        ),
-        TextFormField(
-          textAlign: TextAlign.left,
-          decoration: InputDecoration(
-            fillColor: Colors.red,
-            hintText: 'Password',
-            prefixIcon: Icon(
-              Icons.lock_outline,
-            ),
-            suffixIcon: IconButton(
-              icon: Icon(
-                Icons.remove_red_eye,
-              ),
-              onPressed: () {},
-            ),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(
-            vertical: 16,
-          ),
-          child: TextFormField(
-            textAlign: TextAlign.left,
-            decoration: InputDecoration(
-              fillColor: Colors.red,
-              hintText: 'Confirm Password',
-              prefixIcon: Icon(
-                Icons.lock_outline,
-              ),
-              suffixIcon: IconButton(
-                icon: Icon(
-                  Icons.remove_red_eye,
+    var bloc = context.read<SignupBloc>();
+    return BlocBuilder<SignupBloc, SingupState>(
+      builder: (context, state) {
+        return Column(
+          children: [
+            TextFormField(
+              onChanged: (value) {
+                bloc.add(
+                  FullNameChanged(value),
+                );
+              },
+              textAlign: TextAlign.left,
+              decoration: InputDecoration(
+                fillColor: Colors.red,
+                hintText: 'Full Name',
+                prefixIcon: Icon(
+                  Icons.account_circle_outlined,
                 ),
-                onPressed: () {},
               ),
             ),
-          ),
-        ),
-        ElevatedButton(
-          onPressed: () {},
-          child: Text('signup'),
-        ),
-      ],
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 16,
+              ),
+              child: TextFormField(
+                onChanged: (value) {
+                  bloc.add(
+                    EmailChanged(value),
+                  );
+                },
+                textAlign: TextAlign.left,
+                decoration: InputDecoration(
+                  fillColor: Colors.red,
+                  hintText: 'Phone or Email',
+                  prefixIcon: Icon(
+                    Icons.email_outlined,
+                  ),
+                ),
+              ),
+            ),
+            TextFormField(
+              onChanged: (value) {
+                bloc.add(
+                  PasswordChanged(value),
+                );
+              },
+              textAlign: TextAlign.left,
+              decoration: InputDecoration(
+                fillColor: Colors.red,
+                hintText: 'Password',
+                prefixIcon: Icon(
+                  Icons.lock_outline,
+                ),
+                suffixIcon: IconButton(
+                  icon: Icon(
+                    Icons.remove_red_eye,
+                  ),
+                  onPressed: () {},
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 16,
+              ),
+              child: TextFormField(
+                onChanged: (value) {
+                  bloc.add(
+                    ConfirmPasswordChanged(value),
+                  );
+                },
+                textAlign: TextAlign.left,
+                decoration: InputDecoration(
+                  fillColor: Colors.red,
+                  hintText: 'Confirm Password',
+                  prefixIcon: Icon(
+                    Icons.lock_outline,
+                  ),
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      Icons.remove_red_eye,
+                    ),
+                    onPressed: () {},
+                  ),
+                ),
+              ),
+            ),
+            Text('Dont have account?'),
+          ],
+        );
+      },
     );
   }
 }
--- lib/pages/signup/view/signupPage.dart
+++ lib/pages/signup/view/signupPage.dart
@@ -1,5 +1,7 @@
+import 'package:barux/pages/signup/bloc/bloc.dart';
 import 'package:barux/pages/signup/view/view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SignupPage extends StatelessWidget {
   static const route = '/signup';
@@ -7,15 +9,36 @@ class SignupPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(
-        children: [
-          Signupappbar(),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 48),
-            child: Signupform(),
-          ),
-        ],
+    return BlocProvider(
+      create: (context) => SignupBloc(
+        initialState: SingupState(),
+      ),
+      child: const Scaffold(
+        body: CustomScrollView(
+          slivers: [
+            SliverToBoxAdapter(
+              child: Signupappbar(),
+            ),
+            SliverToBoxAdapter(
+              child: Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 48,
+                ),
+                child: Signupform(),
+              ),
+            ),
+            SliverFillRemaining(
+              hasScrollBody: false,
+              child: Padding(
+                padding: EdgeInsets.fromLTRB(48, 0, 48, 16),
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Signupfooter(),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
