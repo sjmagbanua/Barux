e888aaeb3c6946d157fdf3c38b8bf0db4d3c7b7a
sjmagbanua <snickersjay10@gmail.com>
2025-07-30 18:56:41 +0800

    added footer file

---
+++ lib/pages/signup/view/signupFooter.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Signupfooter extends StatelessWidget {
+  const Signupfooter({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {},
+      child: Text('signup'),
+    );
+  }
+}
