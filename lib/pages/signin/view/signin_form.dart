6cc1db886b8eea2b36876565a3d4e0248698fb79
sjmagbanua <snickersjay10@gmail.com>
2025-08-02 02:37:17 +0800

    added event textfield and wrap blocbuilder prov

--- lib/pages/signin/view/signin_form.dart
+++ lib/pages/signin/view/signin_form.dart
@@ -1,35 +1,50 @@
+import 'package:barux/pages/signin/signin.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SigninForm extends StatelessWidget {
   const SigninForm({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        TextFormField(
-          onChanged: (value) {},
-          textAlign: TextAlign.left,
-          decoration: InputDecoration(
-            fillColor: Colors.red,
-            hintText: 'Email',
-            prefixIcon: Icon(
-              Icons.email_outlined,
+    var bloc = context.read<SigninBloc>();
+
+    return BlocBuilder<SigninBloc, SigninState>(builder: (context, state) {
+      return Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextFormField(
+            onChanged: (value) {
+              bloc.add(
+                EmailChanged(value),
+              );
+            },
+            textAlign: TextAlign.left,
+            decoration: InputDecoration(
+              fillColor: Colors.red,
+              hintText: 'Email',
+              prefixIcon: Icon(
+                Icons.email_outlined,
+              ),
             ),
           ),
-        ),
-        TextFormField(
-          onChanged: (value) {},
-          textAlign: TextAlign.left,
-          decoration: InputDecoration(
-            fillColor: Colors.red,
-            hintText: 'Password',
-            prefixIcon: Icon(
-              Icons.lock_outline,
+          TextFormField(
+            onChanged: (value) {
+              bloc.add(
+                PasswordChanged(value),
+              );
+            },
+            textAlign: TextAlign.left,
+            decoration: InputDecoration(
+              fillColor: Colors.red,
+              hintText: 'Password',
+              prefixIcon: Icon(
+                Icons.lock_outline,
+              ),
             ),
           ),
-        ),
-      ],
-    );
+        ],
+      );
+    });
   }
 }
